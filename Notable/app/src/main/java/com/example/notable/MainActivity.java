7a3ac2eaff08aec846eac92b587d4d69396fef02
Dries Fierens <dries.fierens@student.odisee.be>
2021-10-11 13:49:02 +0200

    Scrapped previous commit and re-add correct Notable app

--- Notable/app/src/main/java/com/example/notable/MainActivity.java
+++ Notable/app/src/main/java/com/example/notable/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.notable;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 

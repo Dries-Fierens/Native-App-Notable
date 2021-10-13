1cfb033eb5717ac8dc10df22acd9b6a965f37e59
Dries Fierens <dries.fierens@student.odisee.be>
2021-10-13 17:24:23 +0200

    Working on my notes page

--- Notable/app/src/main/java/com/example/notable/RegistrationFragment.java
+++ Notable/app/src/main/java/com/example/notable/RegistrationFragment.java
@@ -2,7 +2,6 @@ package com.example.notable;
 
 import android.os.Bundle;
 import android.text.Editable;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

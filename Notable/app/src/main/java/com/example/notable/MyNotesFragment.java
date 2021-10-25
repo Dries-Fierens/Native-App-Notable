3a69dfa898ae5fc7714c958704e48b7cea840e2c
Dries Fierens <dries.fierens@student.odisee.be>
2021-10-25 17:13:27 +0200

    Working on adding a camera

--- Notable/app/src/main/java/com/example/notable/MyNotesFragment.java
+++ Notable/app/src/main/java/com/example/notable/MyNotesFragment.java
@@ -1,19 +1,40 @@
 package com.example.notable;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class MyNotesFragment extends Fragment {
+
+    private FirebaseStorage storage;
+    private StorageReference mStorageRef;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        storage = FirebaseStorage.getInstance();
+        mStorageRef = storage.getReference();
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.notes_fragment, container, false);
@@ -40,11 +61,24 @@ public class MyNotesFragment extends Fragment {
                     ((NavigationHost) getActivity()).navigateTo(new SettingsFragment(), true);
                 }else{
                     /*Camera openen*/
+                    askCameraPermissions();
+
+                    //Gebruik zeker deze video voor ingelogd te blijven!!!
+                    //https://www.youtube.com/watch?v=gD9uQf5UU-g
                 }
                 return true;
             }
         });
 
         return view;
+    }
+
+    private void askCameraPermissions() {
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.CAMERA}, 101);
+        }else{
+            //openCamera();
+            //https://stackoverflow.com/questions/66551781/android-onrequestpermissionsresult-is-deprecated-are-there-any-alternatives
+        }
     }
 }
